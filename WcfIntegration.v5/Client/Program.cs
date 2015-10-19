3e11fb2879208e8712d26a26a365ec728672772a
Ramon Smits <ramon.smits@gmail.com>
2015-10-19 20:41:59 +0200

    Simple performance test.

--- WcfIntegration.v5/Client/Program.cs
+++ WcfIntegration.v5/Client/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
+using System.Linq;
+using System.Threading.Tasks;
 using Messages;
 
 namespace Client
@@ -12,32 +15,38 @@ namespace Client
             Console.WriteLine("This will send requests to the CancelOrder WCF service");
             Console.WriteLine("Press 'Enter' to send a message.To exit, Ctrl + C");
 
-            var orderId = 1;
 
             while (Console.ReadLine() != null)
             {
-                var client = new CancelOrderSvc.WcfServiceOf_CancelOrder_ErrorCodesClient();
-                try
+                Call();
+                var start = Stopwatch.StartNew();
+                Parallel.For(0, 100, i =>
                 {
-                    var message = new CancelOrder
-                    {
-                        OrderId = orderId++
-                    };
+                    Call();
+                });
 
-                    Console.WriteLine("Sending message with OrderId {0}.", message.OrderId);
+                Console.WriteLine("Duration: {0}ms", start.ElapsedMilliseconds);
+            }
+        }
 
-                    client.Process(message);
+        private static int orderId;
 
-                    var returnCode = client.Process(message);
-
-                    Console.WriteLine("Error code returned: " + returnCode);
-
-                    client.Close();
-                }
-                catch
+        private static void Call()
+        {
+            using (var client = new CancelOrderSvc.WcfServiceOf_CancelOrder_ErrorCodesClient())
+            {
+                var message = new CancelOrder
                 {
-                    client.Abort();
-                }
+                    OrderId = System.Threading.Interlocked.Increment(ref orderId)
+                };
+
+                Console.WriteLine("Sending message with OrderId {0}.", message.OrderId);
+
+                client.Process(message);
+
+                var returnCode = client.Process(message);
+
+                Console.WriteLine("Error code returned: " + returnCode);
             }
         }
     }
